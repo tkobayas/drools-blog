f7f46e02280bdcef479df3372d30afd10adac25e
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2019-04-08 18:16:38 +0900

    first example

---
+++ 01_helloworld/src/test/java/org/example/DroolsTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class DroolsTest {
+
+    @Test
+    public void testHello() {
+
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession kSession = kContainer.newKieSession();
+
+        Person john = new Person("john", 25);
+        kSession.insert(john);
+        int fired = kSession.fireAllRules();
+        
+        assertEquals(1, fired);
+
+        kSession.dispose();
+    }
+
+}
